be8b05436578962a677d3a9d0091f6ee6a7f7aac
HXY070103 <225796489+HXY070103@users.noreply.github.com>
2026-08-17 19:33:47 +0800

    Rename the chatbot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Tianyi User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Tianyi.java
@@ -0,0 +1,14 @@
+/**
+ * Runs the Tianyi chatbot application.
+ */
+public class Tianyi {
+    public static void main(String[] args) {
+        String banner = " _____ _                   _\n"
+                + "|_   _(_) __ _ _ __  _   _(_)\n"
+                + "  | | | |/ _` | '_ \\| | | | |\n"
+                + "  | | | | (_| | | | | |_| | |\n"
+                + "  |_| |_|\\__,_|_| |_|\\__, |_|\n"
+                + "                     |___/";
+        System.out.println(banner);
+    }
+}
