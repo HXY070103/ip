5f09eff7a64a03277921175948488863eb531161
HXY070103 <225796489+HXY070103@users.noreply.github.com>
2026-08-18 12:40:07 +0800

    Handle invalid command

--- src/main/java/Tianyi.java
+++ src/main/java/Tianyi.java
@@ -32,6 +32,15 @@ public class Tianyi {
         print(content.toString());
     }
 
+    private static String getArgument(String[] inputParts, String errorMessage)
+            throws TianyiException {
+        if (inputParts.length < 2 || inputParts[1].isBlank()) {
+            throw new TianyiException(errorMessage);
+        }
+
+        return inputParts[1];
+    }
+
     public static void main(String[] args) {
         String banner = " _____ _                   _\n"
                 + "|_   _(_) __ _ _ __  _   _(_)\n"
@@ -50,64 +59,166 @@ public class Tianyi {
 
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine().trim();
-            String[] inputParts = input.split("\\s+", 2);
-            String command = inputParts[0];
 
-            if (command.equals("bye")) {
-                break;
+            if (input.isEmpty()) {
+                continue;
             }
 
-            int taskIdx = 0;
+            try {
+                String[] inputParts = input.split("\\s+", 2);
+                String command = inputParts[0];
 
-            switch (command) {
-                case "":
-                    break;
-                case "list":
-                    printTasks(tasks);
-                    break;
-                case "todo", "deadline", "event":
-                    Task task = null;
-
-                    switch (command) {
-                        case "todo":
-                            task = new ToDo(inputParts[1]);
-                            break;
-                        case "deadline":
-                            String[] deadlineParts = inputParts[1].split("\\s+/by\\s+", 2);
-                            task = new Deadline(deadlineParts[0], deadlineParts[1]);
-                            break;
-                        case "event":
-                            String[] eventParts = inputParts[1].split("\\s+/from\\s+", 2);
-                            String[] timeParts = eventParts[1].split("\\s+/to\\s+", 2);
-                            task = new Event(eventParts[0], timeParts[0], timeParts[1]);
-                            break;
+                if (command.equals("bye")) {
+                    if (inputParts.length > 1 && !inputParts[1].isBlank()) {
+                        throw new TianyiException("The bye command does not accept any arguments.\n"
+                                + "Try: bye");
                     }
 
-                    tasks.add(task);
-                    print("Got it. I've added this task:\n"
-                            + "  " + task.toString() + "\n"
-                            + "Now you have " + tasks.size() + " tasks in the list.");
                     break;
-                case "mark", "unmark":
-                    taskIdx = Integer.parseInt(inputParts[1]) - 1;
+                }
 
-                    switch (command) {
-                        case "mark":
-                            tasks.get(taskIdx).markAsDone();
-                            print("Nice! I've marked this task as done:\n"
-                                    + "  " + tasks.get(taskIdx));
-                            break;
-                        case "unmark":
-                            tasks.get(taskIdx).unmarkAsDone();
-                            print("OK, I've marked this task as not done yet:\n"
-                                    + "  " + tasks.get(taskIdx));
-                            break;
-                    }
-                    break;
-                default:
-                    tasks.add(new Task(input));
-                    print("added: " + input);
-                    break;
+                switch (command) {
+                    // List current tasks
+                    case "list":
+                        if (inputParts.length > 1 && !inputParts[1].isBlank()) {
+                            throw new TianyiException("The list command does not accept any arguments.\n"
+                                    + "Try: list");
+                        }
+
+                        printTasks(tasks);
+                        break;
+
+                    // Add a task
+                    case "todo", "deadline", "event":
+                        String taskFormat = null;
+
+                        switch (command) {
+                            case "todo":
+                                taskFormat = "todo borrow book";
+                                break;
+                            case "deadline":
+                                taskFormat = "deadline return book /by Sunday";
+                                break;
+                            case "event":
+                                taskFormat = "event project meeting /from Mon 2pm /to 4pm";
+                                break;
+                        }
+
+                        if (inputParts.length < 2 || inputParts[1].isBlank()) {
+                            throw new TianyiException("The description of a " + command + " cannot be empty.\n"
+                                    + "Try: " + taskFormat);
+                        }
+
+                        Task task = null;
+
+                        switch (command) {
+                            case "todo":
+                                task = new ToDo(inputParts[1]);
+                                break;
+                            case "deadline":
+                                String[] deadlineParts = inputParts[1].split("\\s+/by\\s+", 2);
+
+                                if (deadlineParts.length < 2 || deadlineParts[1].isBlank()) {
+                                    throw new TianyiException("A deadline must contain /by time.\n"
+                                            + "Try: " + taskFormat);
+                                }
+
+                                task = new Deadline(deadlineParts[0], deadlineParts[1]);
+                                break;
+                            case "event":
+                                boolean hasFrom = inputParts[1].contains("/from");
+                                boolean hasTo = inputParts[1].contains("/to");
+
+                                if (!hasFrom && !hasTo) {
+                                    throw new TianyiException("An event must contain /from time and /to time.\n"
+                                            + "Try: " + taskFormat);
+                                }
+
+                                if (hasFrom && !hasTo) {
+                                    throw new TianyiException("An event with /from must also contain /to time.\n"
+                                            + "Try: " + taskFormat);
+                                }
+
+                                if (!hasFrom && hasTo) {
+                                    throw new TianyiException("An event with /to must also contain /from time.\n"
+                                            + "Try: " + taskFormat);
+                                }
+
+                                if (inputParts[1].indexOf("/to") < inputParts[1].indexOf("/from")) {
+                                    throw new TianyiException("/from time must appear before /to time .\n"
+                                            + "Try: " + taskFormat);
+                                }
+
+                                String[] eventParts = inputParts[1].split("\\s+/from\\s+", 2);
+
+                                if (eventParts.length < 2) {
+                                    throw new TianyiException("An event must contain /from time.\n"
+                                            + "Try: " + taskFormat);
+                                }
+
+                                String[] timeParts = eventParts[1].split("\\s+/to\\s+", 2);
+
+                                if (timeParts.length < 2) {
+                                    throw new TianyiException("An event must contain /to time.\n"
+                                            + "Try: " + taskFormat);
+                                }
+
+                                task = new Event(eventParts[0], timeParts[0], timeParts[1]);
+                                break;
+                        }
+
+                        tasks.add(task);
+                        print("Got it. I've added this task:\n"
+                                + "  " + task + "\n"
+                                + "Now you have " + tasks.size() + " tasks in the list.");
+                        break;
+
+                    // mark or unmark a task
+                    case "mark", "unmark":
+                        if (inputParts.length < 2 || inputParts[1].isBlank()) {
+                            throw new TianyiException("Please specify a task number.\n"
+                                    + "Try: " + command + " 1");
+                        }
+
+                        if (tasks.isEmpty()) {
+                            throw new TianyiException("There are no tasks in your list.\n"
+                                    + "Please add a task.\n"
+                                    + "Try: todo borrow book");
+                        }
+
+                        int taskNumber = 0;
+
+                        try {
+                            taskNumber = Integer.parseInt(inputParts[1]);
+                        } catch (NumberFormatException e) {
+                            throw new TianyiException(inputParts[1] + " is not a valid task number."
+                                    + "Try: " + command + " 1");
+                        }
+
+                        if (taskNumber < 1 || taskNumber > tasks.size()) {
+                            throw new TianyiException("Task number " + taskNumber + " does not exist.\n"
+                                    + "Please enter a number from 1 to " + tasks.size() + "."
+                                    + "Try: " + command + " 1");
+                        }
+
+                        switch (command) {
+                            case "mark":
+                                tasks.get(taskNumber - 1).markAsDone();
+                                print("Nice! I've marked this task as done:\n"
+                                        + "  " + tasks.get(taskNumber - 1));
+                                break;
+                            case "unmark":
+                                tasks.get(taskNumber - 1).unmarkAsDone();
+                                print("OK, I've marked this task as not done yet:\n"
+                                        + "  " + tasks.get(taskNumber - 1));
+                                break;
+                        }
+                        break;
+                    default:
+                        throw new TianyiException("I'm sorry, but I don't know what that means.");
+                }
+            } catch (TianyiException e) {
+                print("Oops! " + e.getMessage());
             }
         }
 
---
+++ src/main/java/TianyiException.java
@@ -0,0 +1,8 @@
+/**
+ * Represents an error caused by an invalid command.
+ */
+public class TianyiException extends Exception {
+    public TianyiException(String message) {
+        super(message);
+    }
+}
