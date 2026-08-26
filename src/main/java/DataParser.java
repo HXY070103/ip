95e4c84d00dff275a0088447d5ee45941a739c4a
HXY070103 <225796489+HXY070103@users.noreply.github.com>
2026-08-26 23:06:20 +0800

    Add storage

---
+++ src/main/java/DataParser.java
@@ -0,0 +1,73 @@
+import java.time.format.DateTimeParseException;
+
+/**
+ * Converts stored task data into task objects.
+ */
+public class DataParser {
+    public Task parse(String data) throws StorageException {
+        String[] dataParts = data.split("\\s*\\|\\s*");
+
+        if (dataParts.length < 3) {
+            throw new StorageException("Invalid task data: " + data);
+        }
+
+        Task task;
+
+        switch (dataParts[0]) {
+            case "T":
+                task = createTodo(dataParts, data);
+                break;
+            case "D":
+                task = createDeadline(dataParts, data);
+                break;
+            case "E":
+                task = createEvent(dataParts, data);
+                break;
+            default:
+                throw new StorageException("Unknown task type: " + data);
+        }
+
+        updateStatus(task, dataParts[1]);
+        return task;
+    }
+
+    private Task createTodo(String[] dataParts, String data) throws StorageException {
+        if (dataParts.length != 3) {
+            throw new StorageException("Invalid todo data: " + data);
+        }
+
+        return new ToDo(dataParts[2]);
+    }
+
+    private Task createDeadline(String[] dataParts, String data) throws StorageException {
+        if (dataParts.length != 4) {
+            throw new StorageException("Invalid deadline data: " + data);
+        }
+
+        try {
+            return new Deadline(dataParts[2], new TaskTime(dataParts[3]));
+        } catch (DateTimeParseException e) {
+            throw new StorageException("Invalid date and time in deadline data: " + dataParts[3]);
+        }
+    }
+
+    private Task createEvent(String[] dataParts, String data) throws StorageException {
+        if (dataParts.length != 5) {
+            throw new StorageException("Invalid event data: " + data);
+        }
+
+        try {
+            return new Event(dataParts[2], new TaskTime(dataParts[3]), new TaskTime(dataParts[4]));
+        } catch (DateTimeParseException e) {
+            throw new StorageException("Invalid date and time in event data: " + data);
+        }
+    }
+
+    private void updateStatus(Task task, String status) throws StorageException {
+        if (status.equals("1")) {
+            task.markAsDone();
+        } else if (!status.equals("0")) {
+            throw new StorageException("Invalid task status: " + status);
+        }
+    }
+}
---
+++ src/main/java/StorageException.java
@@ -0,0 +1,8 @@
+/**
+ * Represents an error encountered while loading or saving tasks.
+ */
+public class StorageException extends TianyiException {
+    public StorageException(String message) {
+        super(message);
+    }
+}
